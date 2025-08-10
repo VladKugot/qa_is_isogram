b194304c37822ac85fc1595676ac62a396d27170
Vlad <vladkygot@gmail.com>
2025-08-10 15:36:58 +0300

    add task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  test.each([
+    ['playgrounds', true],
+    ['look', false],
+    ['Adam', false],
+    ['', true],
+    ['Dermatoglyphics', true],
+    ['Hello', false],
+    ['Python', true],
+  ])('should return %s for "%s"', (word, expected) => {
+    expect(isIsogram(word)).toBe(expected);
+  });
 });
